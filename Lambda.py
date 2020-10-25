c4838b48564a1c2ace6a5744f37b4a1905616bc6
ArbAAz khAn <arbaaz.khan.15cs007@gmail.com>
2020-10-25 19:10:18 +0530

    .

---
+++ Lambda.py
@@ -0,0 +1,23 @@
+addition = lambda x, y: x + y
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+print("Addition:", addition(num1, num2))
+
+substraction = lambda x, y: x - y
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+print("Substraction:", substraction(num1, num2))
+
+multiplication = lambda x, y: x * y
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+print("Multiplication:", multiplication(num1, num2))
+
+division = lambda x, y: x / y
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+print("Division:", division(num1, num2))
